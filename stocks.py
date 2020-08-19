4c644ce8a27951e1e33cba4715e85453f7172baf
Ayush-sharma <57178732+Ayush-sharma-py@users.noreply.github.com>
2020-08-19 11:56:53 +0530

    Commit 5

--- stocks.py
+++ stocks.py
@@ -4,11 +4,11 @@ from tensorflow.keras import layers
 import yfinance as yf
 import numpy
 
-tsla = yf.Ticker("TSLA")
-history = tsla.history(period = "max")
+stock = yf.Ticker(input("Stock Code : "))
+history = stock.history(period = "max")
 
-prices = []
-index = []
+stock_prices = []
+stock_index = []
 
 
 for i in history["Close"]:
@@ -17,20 +17,4 @@ for i in history["Close"]:
 for i in range(1,len(prices) + 1):
     index.append(i)
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
-])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-'''
-prices = numpy.array(prices)
-index = numpy.array(index)
-'''
-model.fit(prices,index, epochs=10)
-
-probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-predictions = probability_model.predict(index[-1] + 1)
